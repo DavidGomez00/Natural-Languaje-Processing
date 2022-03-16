50030a4e2beeb714ccfa9155707cdbe0655a5de9
David Gómez Ortiz <dvdgmzrtz@gmail.com>
2022-03-16 15:33:11 +0100

    a

--- Assignment2/LaplaceUnigramLanguageModel.py
+++ Assignment2/LaplaceUnigramLanguageModel.py
@@ -42,7 +42,9 @@ class LaplaceUnigramLanguageModel:
     score = 0.0
     # For each word
     for token in sentence:
-      # Compute score
-      count = self.unigramCounts[token] + 1
-      score += math.log(count/(self.total+len(self.words))) 
+      # Exclude <s> and </s>
+      if token != "<s>" and token != "</s>":
+        # Compute score
+        count = self.unigramCounts[token] + 1
+        score += math.log(count/(self.total+len(self.words)))
     return score
--- Assignment2/SpellCorrect.py
+++ Assignment2/SpellCorrect.py
@@ -114,13 +114,13 @@ def main():
   uniformSpell = SpellCorrect(uniformLM, trainingCorpus)
   uniformOutcome = uniformSpell.evaluate(devCorpus) 
   print(str(uniformOutcome))
-
+  
   print('Laplace Unigram Language Model: ') 
   laplaceUnigramLM = LaplaceUnigramLanguageModel(trainingCorpus)
   laplaceUnigramSpell = SpellCorrect(laplaceUnigramLM, trainingCorpus)
   laplaceUnigramOutcome = laplaceUnigramSpell.evaluate(devCorpus)
   print(str(laplaceUnigramOutcome))
-
+  '''
   print('Laplace Bigram Language Model: ')
   laplaceBigramLM = LaplaceBigramLanguageModel(trainingCorpus)
   laplaceBigramSpell = SpellCorrect(laplaceBigramLM, trainingCorpus)
@@ -138,6 +138,6 @@ def main():
   knSpell = SpellCorrect(knLM, trainingCorpus)
   knOutcome = knSpell.evaluate(devCorpus)
   print(str(knOutcome))
-  
+  '''
 if __name__ == "__main__":
     main()
